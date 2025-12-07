793fcb3f0416cc1ae44daab3a211ae4a215041bf
Luis Alonso <luisalonso97@gmail.com>
2025-12-07 17:59:31 -0600

    Changed approach to invalid id gen instead of brute force

--- 02_Gift_Shop/main.py
+++ 02_Gift_Shop/main.py
@@ -1,17 +1,26 @@
-from dataclasses import dataclass
 import re
-
 import utils.file_reader as fr
 
 
-@dataclass
-class IdRange:
-    min_count: int
-    max_count: int
+def generate_invalid_ids(max_val: int) -> set[int]:
+    invalid_ids = set()
+    max_digits = len(str(max_val))
 
+    for pattern_len in range(1, max_digits // 2 + 1):
+        start_pat = 10 ** (pattern_len - 1)
+        end_pat = 10**pattern_len
 
-def is_id_pattern_valid(id: str) -> bool:
-    return id[: len(id) // 2] != id[len(id) // 2 :]
+        for pat in range(start_pat, end_pat):
+            pat_str = str(pat)
+
+            current_str = pat_str * 2
+            while len(current_str) <= max_digits:
+                val = int(current_str)
+                if val <= max_val:
+                    invalid_ids.add(val)
+                current_str += pat_str
+
+    return invalid_ids
 
 
 def main() -> None:
@@ -20,14 +29,26 @@ def main() -> None:
         "inputs/02.dat", parse_exp, re.findall
     )
 
-    sum_of_invalid_ids: int = 0
+    ranges: list[tuple[int, int]] = []
+    max_id_needed: int = 0
 
     for match in data[0]:
-        id_range: IdRange = IdRange(int(match[0]), int(match[1]))
-        ids = (x for x in range(id_range.min_count, id_range.max_count))
-        for id in ids:
-            if not is_id_pattern_valid(str(id)):
-                sum_of_invalid_ids += id
+        start = int(match[0])
+        end = int(match[1])
+        ranges.append((start, end))
+        max_id_needed = max(max_id_needed, end)
+
+    invalid_ids = generate_invalid_ids(max_id_needed)
+
+    sum_of_invalid_ids: int = 0
+
+    sorted_invalid_ids = sorted(list(invalid_ids))
+
+    for val in sorted_invalid_ids:
+        for start, end in ranges:
+            if start <= val < end:
+                sum_of_invalid_ids += val
+                break
 
     print(sum_of_invalid_ids)
 
